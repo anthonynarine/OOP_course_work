ee9df0c8ca2dbd817601c348436bda114be74ecd
Anthony Narine <anarine83@gmail.com>
2022-09-07 21:57:01 -0400

    Dockstrings added and created method to give a raise

--- main.py
+++ main.py
@@ -13,13 +13,22 @@ class Employee:
         self.salary = annual_salary
 
     def get_name (self):
+        """Method to obtain an employee name"""
         return self.name
 
     def get_age(self):
+        """Method to obtain an employee's age"""
         return self.age
 
     def get_salary(self):
+        """ Method to obtain an employee salary"""
         return self.salary
+
+    def get_raise(self, bonus):
+        """Method to give an employee a raise"""
+        self.salry += bonus
+
+    
     
 
 
@@ -28,10 +37,12 @@ e2 = Employee ("Jessica", 39, 100_000)
 print (e1.get_name())
 print (e1.get_age())
 print (e1.get_salary())
+print (id(e1))
 
 print (e2.get_name())
-print (e2.get_age)
+print (e2.get_age())
 print (e2.get_salary())
+print (id(e2))
 
         
 
